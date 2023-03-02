8de32a1641163176793dca0e5bfdeae6181915f1
Clinton Ogada <clintonogada@gmail.com>
2023-03-02 23:47:30 +0300

    Update SmartDevice.kt
    Updated the 'turnOff()' method

--- SmartDevice.kt
+++ SmartDevice.kt
@@ -1,10 +1,17 @@
+//This is the class instantiation
 class SmartDevice {
-    fun turnOn(){
+        fun turnOn(){
+        
         println("Smart device is turned on.")
     }
+    fun turnOff(){
+        println("Smart device is turned off.")
 }
-
+//this is the main method
 fun main() {
     val device = SmartDevice()
+    //This method turns on the device
     device.turnOn()
+    //This method turns off the device
+    device.turnOff()
 }
